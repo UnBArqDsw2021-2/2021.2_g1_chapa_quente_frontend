5a6288892c4bcf83d5cf38364f055164a043cc38
Pedro Lima <phlimas@outlook.com>
2022-03-06 17:47:25 -0300

    Ajuste na grid style do projeto e integrando api para teste!
    Co-authored-by: Philipe <philipeserafim@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import api from './services';
 
 function App() {
+  const [msg, setMsg] = useState()
+
+  const testApi = async () => {
+    const res = await api.get('/');
+    setMsg(res.data.message);
+  };
+
+  testApi()
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload
+          {msg}
         </p>
         <a
           className="App-link"
