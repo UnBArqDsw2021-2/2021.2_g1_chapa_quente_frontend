29799107f3ec6555675fa1b1d2f12b5eca815d6b
DafneM <moretti.dafne@gmail.com>
2022-03-22 21:15:53 -0400

    Adicionado card de refeição
    Co-authored-by:  Giulia <giulialobo89@gmail.com>

---
+++ src/components/RefeicaoCard/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
+
+export const RefeicaoCard = () => {
+  <div>
+      <p>Card de refeição</p>
+  </div>
+};
