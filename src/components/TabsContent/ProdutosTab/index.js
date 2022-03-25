51b08582b3d748c61b4aa0daeb967b0f4b8e0ffb
philipeserafim <philipeserafim@gmail.com>
2022-03-25 17:50:46 -0300

    Iniciando componente de produto
    Co-authored-by: Pedro Lima <phlimas@outlook.com>

--- src/components/TabsContent/ProdutosTab/index.js
+++ src/components/TabsContent/ProdutosTab/index.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const ProdutosTab = () => {
 
+  const [produtos, setProdutos] = useState();
+
+  const getData = async () => {
+    await fetch('https://it3-hbn-default-rtdb.firebaseio.com/ovosPascoa.json')
+      .then(response => response.json())
+      .then(json => {
+        setProdutos(json);
+      })
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="produtos-tab-div">
-      <p>PRODUTOS</p>
+      <ul className="produtos-lista">
+        <li className="produtos-item">
+          {}
+          {console.log(produtos)}
+        </li>
+      </ul>
     </div>
   );
 
