d6a88775fe40fb7508fe17ae86da38b635cb6bbf
Pedro Lima <phlimas@outlook.com>
2022-03-31 19:54:37 -0300

    Resolvendo problema ao receber dados do back
    Co-authored-by: Pedro <phlimas@outlook.com>

--- src/components/NavTab/index.js
+++ src/components/NavTab/index.js
@@ -1,51 +1,83 @@
-import React, { useState, useEffect } from "react";
-import './style.css'
-import { NavTabItem } from '../NavTabItem'
-import { NavTabContent } from "../NavTabContent";
-import { ProdutosTab } from "../TabsContent/ProdutosTab"
-import { UsuariosTab } from "../TabsContent/UsuariosTab"
+import React, { useState, useEffect } from 'react';
+import './style.css';
+
+import { NavTabItem } from '../NavTabItem';
+import { NavTabContent } from '../NavTabContent';
+// import { ProdutosTab } from "../TabsContent/ProdutosTab"
+import { UsuariosTab } from '../TabsContent/UsuariosTab';
+import api from '../../services';
+import { Loading } from '../Loading';
 
 export const NavTab = () => {
   const token = JSON.parse(sessionStorage.getItem('@token'));
 
   const [produtos, setProdutos] = useState();
+  const [load, setload] = useState(true);
 
   const getData = async () => {
-    await fetch('http://localhost:3333/cliente/all', {
-      method: 'get',
-      headers: {
-        authorization: `Bearer ${token}`
-      }
-    })
-      .then(response => response.json())
-      .then(json => {
-        setProdutos(json);
-      })
+    try {
+      const response = await api.get('/cliente/all', {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      });
+
+      setProdutos(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
     getData();
   }, []);
 
-  const [activeTab, setActiveTab] = useState("produtos");
+  useEffect(() => {
+    if (produtos !== undefined) {
+      setload(false);
+    } else {
+      setload(true);
+    }
+  }, [produtos]);
+
+  const [activeTab, setActiveTab] = useState('produtos');
   return (
-    <div className="nav-tab-div">
-      <ul className="nav">
-        <li>
-          <NavTabItem title="Produtos" id="produtos" activeTab={activeTab} setActiveTab={setActiveTab} />
-        </li>
-        <li>
-          <NavTabItem title="Usuários" id="usuarios" activeTab={activeTab} setActiveTab={setActiveTab} />
-        </li>
-      </ul>
-      <div className="outlet">
-        <NavTabContent id="produtos" activeTab={activeTab}>
-          <ProdutosTab produtos={produtos !== undefined ? produtos : []}/>
-        </NavTabContent>
-        <NavTabContent id="usuarios" activeTab={activeTab}>
-          <UsuariosTab />
-        </NavTabContent>
-      </div>
-    </div>
+    <>
+      {load ? (
+        <Loading />
+      ) : (
+        <div className="nav-tab-div">
+          <ul className="nav">
+            <li>
+              <NavTabItem
+                title="Produtos"
+                id="produtos"
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
+            </li>
+            <li>
+              <NavTabItem
+                title="Usuários"
+                id="usuarios"
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
+            </li>
+          </ul>
+          <div className="outlet">
+            <NavTabContent id="produtos" activeTab={activeTab}>
+              {/* <ProdutosTab produtos={produtos !== undefined ? produtos : []}/> */}
+              {produtos.map(data => (
+                <p>{data.nome}</p>
+              ))}
+            </NavTabContent>
+            <NavTabContent id="usuarios" activeTab={activeTab}>
+              <UsuariosTab />
+            </NavTabContent>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
