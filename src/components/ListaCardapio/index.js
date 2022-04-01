74d9b921716f80cc78a4823254dca4a7d281fe5d
Giulia <giulialobo89@gmail.com>
2022-04-01 01:28:12 -0300

    Adicionado tela de loading e integração do cardapio e ajuste de detalhes de css e de responsividade

--- src/components/ListaCardapio/index.js
+++ src/components/ListaCardapio/index.js
@@ -2,47 +2,69 @@ import React, { useEffect, useState } from 'react';
 import './style.css';
 import { RefeicaoCard } from '../RefeicaoCard';
 import api from '../../services';
+import { Loading } from '../Loading';
 
 export const ListaCardapio = () => {
-  const token = JSON.parse(sessionStorage.getItem('@token'));
-
-  console.log(token);
-
-  // const navigate = useNavigate();
-
   const [sanduiches, setSanduiches] = useState([]);
   const [bebidas, setBebidas] = useState([]);
   const [acompanhamentos, setAcompanhamentos] = useState([]);
+  const [mergeAll, setMergeAll] = useState([]);
+  const [load, setLoad] = useState(true);
 
   const getCardapioItensFromApi = async type => {
-    const result = await api.get(`/${type}`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    const result = await api.get(`/${type}`);
     if (result.status === 200) {
       return result.data;
-      // setMsgSuccess(true);
     }
     return result.data.error;
   };
 
   useEffect(() => {
-    getCardapioItensFromApi('sanduiche').then(result => setSanduiches(result));
-    getCardapioItensFromApi('bebida').then(result => setBebidas(result));
-    getCardapioItensFromApi('acompanhamento').then(result =>
-      setAcompanhamentos(result),
-    );
+    try {
+      getCardapioItensFromApi('sanduiche').then(result =>
+        setSanduiches(result),
+      );
+      getCardapioItensFromApi('bebida').then(result => setBebidas(result));
+      getCardapioItensFromApi('acompanhamento').then(result =>
+        setAcompanhamentos(result),
+      );
+    } catch (error) {
+      console.log(error);
+    }
   }, []);
-  console.log(sanduiches, bebidas, acompanhamentos);
+
+  useEffect(() => {
+    if (
+      bebidas.length !== 0 ||
+      sanduiches.length !== 0 ||
+      acompanhamentos.length !== 0
+    ) {
+      setMergeAll([...bebidas, ...sanduiches, ...acompanhamentos]);
+    }
+  }, [sanduiches, bebidas, acompanhamentos]);
+
+  useEffect(() => {
+    if (Object.keys(mergeAll).length > 0) {
+      setLoad(false);
+    }
+  }, [mergeAll]);
 
   return (
     <>
-      <div className="container-list">
-        <div className="cards">
-          {sanduiches.map(sanduiche => (
-            <RefeicaoCard name={sanduiche.descricao} price={sanduiche.preco} />
-          ))}
+      {load ? (
+        <Loading />
+      ) : (
+        <div className="container-list">
+          <div className="cards">
+            {mergeAll.map(sanduiche => (
+              <RefeicaoCard
+                name={sanduiche.descricao}
+                price={sanduiche.preco}
+              />
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
