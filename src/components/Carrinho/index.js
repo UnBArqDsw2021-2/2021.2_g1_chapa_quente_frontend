0fa396e45aba7d38f3a8333542c125bed1482b70
João Pedro Chaves <joaopedroaschaves@gmail.com>
2022-04-06 19:20:41 -0300

    Adicionada base para tela de carrinho

---
+++ src/components/Carrinho/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './style.css';
+
+export const Carrinho = () => {
+
+
+    return (
+        <div className="carrinho-container">
+            <div className="carrinho-card">
+                <div className="carrinho-title">
+                    Carrinho
+                </div>
+                <div className="carrinho-inside-card" >
+                    <div className="carrinho-box">
+                        {funcao de mapping}
+                    </div>
+                    <div className="carrinho-confirm-button">
+                        Confirmar Pedido
+                    </div>
+                </div>
+            </div> 
+        </div>
+    );
+};
